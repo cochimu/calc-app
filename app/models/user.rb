39004fae3527ee778850ec9c51aeda609c315960
cochimu <cochimu1221@icloud.com>
2021-02-28 16:48:54 +0900

    devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+      
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } do
+    validates :password
+    validates :password_confirmation
+  end
+  
+end
